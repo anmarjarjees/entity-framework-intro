14a738a04f6976b4bbf4ce068ee41401011a842f
Anmar PC <aj@anmarjarjees.com>
2023-07-20 18:41:18 -0400

    Using LocalDB-SQLServer

---
+++ EFGetMore/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EFGetMore
+{
+    /*
+    You can follow the same code logic, create your local database
+    using LocalDB for the "SQL Server" and Open it within Visual Studio DB Design.
+    Please refer to my PDF files.
+    */ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
